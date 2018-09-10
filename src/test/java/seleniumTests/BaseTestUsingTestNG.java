ecd670fe0358adee6eccc14a31f5a2d71bb73130
quan612 <quan612@yahoo.com>
2018-09-10 13:48:30 -0400

    update code

--- src/test/java/seleniumTests/BaseTestUsingTestNG.java
+++ src/test/java/seleniumTests/BaseTestUsingTestNG.java
@@ -60,7 +60,7 @@ public class BaseTestUsingTestNG {
 	  
 	  System.setProperty("webdriver.gecko.driver","C:\\Cucumber\\geckodriver.exe");
 	  //System.setProperty("webdriver.gecko.driver","J:\\Automation 2018\\geckodriver.exe");
-	  System.setProperty("webdriver.firefox.bin","C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+	  //System.setProperty("webdriver.firefox.bin","C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
 	  //System.setProperty("webdriver.firefox.bin","C:\\Users\\Quan.Huynh\\AppData\\Local\\Nightly\\firefox.exe");
 	  
 	  //System.setProperty("webdriver.chrome.driver","C:\\Cucumber\\chromedriver.exe");
