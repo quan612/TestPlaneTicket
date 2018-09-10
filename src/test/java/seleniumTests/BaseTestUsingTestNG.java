569432c0c48c6b8b253bf78b60a9d186a0660a18
quan612 <quan612@yahoo.com>
2018-09-10 16:31:00 -0400

    update

--- src/test/java/seleniumTests/BaseTestUsingTestNG.java
+++ src/test/java/seleniumTests/BaseTestUsingTestNG.java
@@ -21,82 +21,81 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 
 public class BaseTestUsingTestNG {
-	
+
 	protected static WebDriver driver;	
-	
-  //@Test
-  //public void Test1() throws InterruptedException {		 	  
-	  
-	
+
+	//@Test
+	//public void Test1() throws InterruptedException {		 	  
+
+
 	//  FlightHomePageTest flightHomeTest = new FlightHomePageTest(driver);
 	//  Assert.assertTrue(flightHomeTest.VerifyUserIsPresentFlightTab(), "User is not present flight tab, please recheck!!!");
-	  
-	 // FlightHomePage flh = new FlightHomePage(driver);
-	 // Assert.assertTrue(flh.VerifyUserIsPresentFlightTab(), "User is not present flight tab, please recheck!!!");
-	  //  FlightHomePage flh = new FlightHomePage(driver);
-	  
-	  
-//	  flh.EnterToDestination(strToDestination);
-//	  flh.PickingDepartDate(strDepartDate);
-//	  flh.PickingReturnDate(strReturnDate);	 
-//	  flh.SearchFlightAndGoToFlightFilterPage();	  
-//	
-//	  //now it is at flight filter page	  
-//	  FlightFilterPage flf = new FlightFilterPage(driver);
-//	//  Assert.assertEquals(flf.VerifyIfSearchFareModalDialogExist(),(Boolean) true);
-//	  //Assert.assertEquals(flf.VerifySearchCheapestFaresText(), (Boolean) true);
-//	  flf.WaitForFlightResultLoad();
-//	  flf.ClickOnSelectAllAirlinesCheckBox();
-//	  flf.ClickOnShowMoreToSeeAirlineOptions();
-//	  flf.SelectPreferAirlineBrand(strAirlinePrefer);
-//	  flf.WaitForFlightResult();	
-//	  flf.GetFirstPriceInFlightResultDetail();
-	  
-  //}
 
-  @BeforeMethod
-  public void beforeMethod() {
-	  System.out.println("Starting web driver!");
-	  
-	  //System.setProperty("webdriver.gecko.driver","C:\\Cucumber\\geckodriver.exe");
-	  System.setProperty("webdriver.gecko.driver","geckodriver/geckodriver");
-	  //System.setProperty("webdriver.gecko.driver","J:\\Automation 2018\\geckodriver.exe");
-	  //System.setProperty("webdriver.firefox.bin","C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
-	  //System.setProperty("webdriver.firefox.bin","C:\\Users\\Quan.Huynh\\AppData\\Local\\Nightly\\firefox.exe");
-	  
-	  //System.setProperty("webdriver.chrome.driver","C:\\Cucumber\\chromedriver.exe");
-	 System.setProperty("webdriver.chrome.driver","chromedriver");
-	 // FirefoxOptions options = new FirefoxOptions().addPreference("browser.link.open_newwindow", 1);  
-	  //options.setHeadless(true);
-	  //driver = new FirefoxDriver(options);
-	 
-	 
-	 ChromeOptions options = new ChromeOptions();
-     
-     options.addArguments("--no-sandbox");
-     options.addArguments("--disable-dev-shm-usage");
-      driver = new ChromeDriver(options);
-      //Put a Implicit wait, this means that any search for elements on the page could take the time the implicit wait is set for before throwing exception
+	// FlightHomePage flh = new FlightHomePage(driver);
+	// Assert.assertTrue(flh.VerifyUserIsPresentFlightTab(), "User is not present flight tab, please recheck!!!");
+	//  FlightHomePage flh = new FlightHomePage(driver);
 
-      //driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
-      driver.get("https://www.flighthub.com/");
-      
-      // test
-      
-  /*    System.setProperty("webdriver.chrome.driver", "C:\\Cucumber\\chromedriver.exe");
+
+	//	  flh.EnterToDestination(strToDestination);
+	//	  flh.PickingDepartDate(strDepartDate);
+	//	  flh.PickingReturnDate(strReturnDate);	 
+	//	  flh.SearchFlightAndGoToFlightFilterPage();	  
+	//	
+	//	  //now it is at flight filter page	  
+	//	  FlightFilterPage flf = new FlightFilterPage(driver);
+	//	//  Assert.assertEquals(flf.VerifyIfSearchFareModalDialogExist(),(Boolean) true);
+	//	  //Assert.assertEquals(flf.VerifySearchCheapestFaresText(), (Boolean) true);
+	//	  flf.WaitForFlightResultLoad();
+	//	  flf.ClickOnSelectAllAirlinesCheckBox();
+	//	  flf.ClickOnShowMoreToSeeAirlineOptions();
+	//	  flf.SelectPreferAirlineBrand(strAirlinePrefer);
+	//	  flf.WaitForFlightResult();	
+	//	  flf.GetFirstPriceInFlightResultDetail();
+
+	//}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Starting web driver!");
+
+		//System.setProperty("webdriver.gecko.driver","C:\\Cucumber\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver","geckodriver/geckodriver");
+		//System.setProperty("webdriver.gecko.driver","J:\\Automation 2018\\geckodriver.exe");
+		//System.setProperty("webdriver.firefox.bin","C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+		//System.setProperty("webdriver.firefox.bin","C:\\Users\\Quan.Huynh\\AppData\\Local\\Nightly\\firefox.exe");
+
+		//System.setProperty("webdriver.chrome.driver","C:\\Cucumber\\chromedriver.exe");
+		
+		// FirefoxOptions options = new FirefoxOptions().addPreference("browser.link.open_newwindow", 1);  
+		//options.setHeadless(true);
+		//driver = new FirefoxDriver(options);
+
+		System.setProperty("webdriver.chrome.driver","chromedriver");
+		ChromeOptions options = new ChromeOptions();     
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		driver = new ChromeDriver(options);
+		//Put a Implicit wait, this means that any search for elements on the page could take the time the implicit wait is set for before throwing exception
+
+		//driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+		driver.get("https://www.flighthub.com/");
+
+		// test
+
+		/*    System.setProperty("webdriver.chrome.driver", "C:\\Cucumber\\chromedriver.exe");
 		driver = new ChromeDriver();
 		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("http://google.com");		
-	*/	
-  }
+		 */	
+	}
 
-  @AfterMethod
-  public void afterMethod() 
-  {
-	  // Close the driver
-	  System.out.println("closing driver ");
-	  //driver.close();
-    driver.quit();
-  }
+	@AfterMethod
+	public void afterMethod() 
+	{
+		// Close the driver
+		System.out.println("closing driver ");
+		//driver.close();
+		driver.quit();
+	}
 
 }
