a37ed8276afc84c38e4a3f57cd149e83ae160a9c
Azure <quan612@yahoo.com>
2018-09-24 23:01:19 -0400

    travis update

--- src/test/java/cucumber/TestRunner/CucumberRunner.java
+++ src/test/java/cucumber/TestRunner/CucumberRunner.java
@@ -1,41 +1,33 @@
 package cucumber.TestRunner;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.CucumberFeatureWrapper;
 import cucumber.api.testng.TestNGCucumberRunner;
 
-import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
-import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
-import com.github.mkolisnyk.cucumber.runner.ExtendedTestNGRunner;
+//import com.github.mkolisnyk.cucumber.runner.*;
 
-//@ExtendedCucumberOptions(jsonReport = "target/cucumber.json"
-//						,overviewReport = true
-//						,detailedReport = true
-//						,detailedAggregatedReport = true
-//						,outputFolder = "target")
 
-@CucumberOptions(
-		features="src/test/resources/features"		
-		,glue={"cucumber.StepDefinitions"}
-		,tags= {"@scenario"}
+/* for mkolisnyk report
+@ExtendedCucumberOptions(jsonReport = "target/cucumber.json"
+						,overviewReport = true
+						,detailedReport = true
+						,detailedAggregatedReport = true
+						,outputFolder = "target")
+
 		//,plugin = { "html:target/cucumber-html-report" 
 		//		,"json:target/cucumber.json", "pretty:target/cucumber-pretty.txt"
 		//        ,"usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml"}
+*/		
+@CucumberOptions(
+		features="src/test/resources/features"		
+		,glue={"cucumber.StepDefinitions"}
+		,tags= {"@scenario"}		
 		,plugin = {"json:target/cucumber-report/cucumber.json"}
 		,monochrome = true) 
 
@@ -43,15 +35,6 @@ public class CucumberRunner //extends ExtendedTestNGRunner //extends ExtendedTes
 {
 	public static WebDriver driver;
 	private TestNGCucumberRunner testRunner;
-
-	@BeforeSuite
-	public static void suiteSetUp() {
-		// TODO: Add setup code
-	}
-	@AfterSuite
-	public static void suiteTearDown() {
-		// TODO: Add tear down code
-	}
 		
 	@BeforeTest
 	//@BeforeMethod
@@ -62,19 +45,19 @@ public class CucumberRunner //extends ExtendedTestNGRunner //extends ExtendedTes
 		
 		//System.setProperty("webdriver.gecko.driver","C:\\Cucumber\\geckodriver.exe");
 		System.setProperty("webdriver.gecko.driver","geckodriver/geckodriver");
+		//System.setProperty("webdriver.gecko.driver","J:\\Automation 2018\\geckodriver.exe");	
 		
 		//driver = new ChromeDriver();
 		//System.setProperty("webdriver.firefox.bin","C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");		
 		FirefoxOptions options = new FirefoxOptions();
 		options.addPreference("browser.link.open_newwindow", 1); 
-		//options.setHeadless(true);
+		options.setHeadless(true);
 		
 		driver = new FirefoxDriver(options);
 		testRunner = new TestNGCucumberRunner(CucumberRunner.class);			
 	}
 	
-//	/*
-	//@Test(description="login",dataProvider="features")
+//  /*		
 	@Test(dataProvider="features")
 	public void UserCanSearchForFlightTicket(CucumberFeatureWrapper cFeature)
 	{
@@ -97,5 +80,3 @@ public class CucumberRunner //extends ExtendedTestNGRunner //extends ExtendedTes
 		driver.quit();
 	}	
 }
-
-//*/
--- src/test/resources/features/UserCanSearchFlightTicket.feature
+++ src/test/resources/features/UserCanSearchFlightTicket.feature
@@ -1,42 +1,3 @@
-#Author: your.email@your.domain.com
-#Keywords Summary :
-#Feature: List of scenarios.
-#Scenario: Business rule through list of steps with arguments.
-#Given: Some precondition step
-#When: Some key actions
-#Then: To observe outcomes or validation
-#And,But: To enumerate more Given,When,Then steps
-#Scenario Outline: List of steps for data-driven as an Examples and <placeholder>
-#Examples: Container for s table
-#Background: List of steps run before each of the scenarios
-#""" (Doc Strings)
-#| (Data Tables)
-#@ (Tags/Labels):To group Scenarios
-#<> (placeholder)
-#""
-## (Comments)
-#Sample Feature Definition Template
-#@tag
-#Feature: Title of your feature
-#  I want to use this template for my feature file
-# @tag1
-#  Scenario: Title of your scenario
-#    Given I want to write a step with precondition
-#   And some other precondition
-#   When I complete action
-#  And some other action
-# And yet another action
-#  Then I validate the outcomes
-#  And check more outcomes
-# @tag2
-# Scenario Outline: Title of your scenario outline
-#   Given I want to write a step with <name>
-#   When I check for the <value> in step
-#   Then I verify the <status> in step
-#   Examples:
-#     | name  | value | status  |
-#    | name1 |     5 | success |
-#     | name2 |     7 | Fail    |
 Feature: User Can search for Flight ticket
 
 @scenario
@@ -45,12 +6,21 @@ Feature: User Can search for Flight ticket
     When user clicks on search flight button
     Then user is presented with validation errors asking to enter flight information
     
-#@scenario
-  Scenario: User is present with flight price after searching for ticket
+@scenario
+  Scenario: User can see different airline brands after searching for a ticket
     Given the user is on flight home page
     When user enters flight destination
 	And user enters the date he wants to depart
 	And user enteres the date he wants to return
 	And user clicks on search flight button
-	Then user is show all available flight tickets within that time period
+	Then user is show all available flight tickets and available airline brand
 	
+@scenario
+  Scenario: User can see the cheapest ticket for a certain airline brand
+    Given the user is on flight home page
+    When user enters flight destination
+	And user enters the date he wants to depart
+	And user enteres the date he wants to return
+	And user clicks on search flight button
+	And user select prefer airline brand
+	Then user can see the cheapest ticket of that airline brand
